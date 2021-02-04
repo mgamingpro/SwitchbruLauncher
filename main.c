9398744172d3eac4bd24cc84bd853562e0b0d92b
Muhammad <muhammadmasoom1001@gmail.com>
2021-02-04 18:16:16 +0000

    Adding source code

---
+++ main.c
@@ -0,0 +1,96 @@
+// Include the most common headers from the C standard library
+// This is an edit of LennyBrew to launch SwitchBru, this is in now affiliated with Switchbru (c)
+/*  Switchbru Launcher
+    Copyright (C) 2021, Syntax64 "Syntax 64" 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation at version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    In addition, the following extra clauses are in effect:
+
+    7b) Requiring preservation of specified reasonable legal notices or
+    author attributions in that material or in the Appropriate Legal
+    Notices displayed by works containing it;
+
+    7c) Prohibiting misrepresentation of the origin of that material, or
+    requiring that modified versions of such material be marked in
+    reasonable ways as different from the original version;
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Include the main libnx system header, for Switch development
+#include <switch.h>
+
+void openSwitchBru() 
+{
+    Result rc;
+    WebCommonConfig config;
+
+    // Create the config.
+    rc = webPageCreate(&config, "http://dns.switchbru.com/");
+
+    if (R_SUCCEEDED(rc)) {
+        rc = webConfigSetWhitelist(&config, "^http*"); // Keeping the whitelist matching everything (cleaner/less annoying).
+        if (R_SUCCEEDED(rc)) { // Launch the applet and wait for it to exit.
+            rc = webConfigShow(&config, NULL);
+        } else {
+            printf("Something went wrong opening the browser.");
+        }
+    }
+    else {
+        printf("Something went wrong making the config object.");
+    }
+}
+
+// Main program entrypoint
+int main(int argc, char* argv[])
+{
+    consoleInit(NULL);
+
+    printf("Switchbru\n\nPress PLUS to close.\n");
+
+    bool isApp = true;
+    if (!(appletGetAppletType() == AppletType_Application)) {
+        printf(CONSOLE_RED "Switchbru Launcher is not running in application mode.\n");
+        printf("Launch the launcher from application mode instead.\n");
+        printf(CONSOLE_YELLOW "If you are using Atmosphere,\nlaunch via holding R while opening an application\n\n");
+        isApp = false;
+    } else {
+        printf(CONSOLE_GREEN "Press A to relaunch Switchbru Launcher.\n\n");
+    }
+    printf(CONSOLE_RESET "Copyright (c) Syntax64 \"Syntax 64\" V., 2021\n\nReleased under the AGPLv3.\nNot licensable under later versions and clause 7b and 7c are in effect.");
+
+    if (isApp)
+        openSwitchBru();
+    // Main loop
+    while (appletMainLoop())
+    {
+        // Scan all the inputs. This should be done once for each frame
+        hidScanInput();
+
+        // hidKeysDown returns information about which buttons have been
+        // just pressed in this frame compared to the previous one
+        u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+
+        if (kDown & KEY_PLUS)
+            break;
+
+        if (kDown & KEY_A)
+            if (isApp)
+                openSwitchBru();
+        consoleUpdate(NULL);
+    }
+    consoleExit(NULL);
+    return 0;
+}
